de2d7fa9a1f96c1985d076516cc70412ac16e140
boamahdavid48-prog <boamahdavid48@gmail.com>
2026-05-31 20:40:50 +0000

    Completed initials assignment

--- initials.py
+++ initials.py
@@ -1,7 +1,38 @@
+# File: initials.py
+# Description: Print out my initials in stylized large block letters.
+# Assignment Number: 1
+#
+# Name: David Boamah
+# STUDENT ID: 2425403979
+# Email: 2425403979@live.gctu.edu.gh
+# Grader: Augustus Buckman
+#
+# On my honor, David Boamah, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
 
 def main():
-  pass #code goes here
+    
 
-
+    print()
+    print("...DDB")
+    print()
+    print("...DDDDDDDDDD..........DDDDDDDDDD..........BBBBBBBBBB.......")
+    print("...DD......DDDD........DD......DDDD........BB......BBBB.....")
+    print("...DD.......DDD........DD.......DDD........BB.......BBB.....")
+    print("...DD........DD........DD........DD........BB......BBBB.....")
+    print("...DD........DD........DD........DD........BBBBBBBBB........")
+    print("...DD........DD........DD........DD........BB......BBBB.....")
+    print("...DD........DD........DD........DD........BB.......BBB.....")
+    print("...DD.......DDD........DD.......DDD........BB........BB.....")
+    print("...DD......DDDD...**...DD......DDDD...**...BB......BBBB...**")
+    print("...DDDDDDDDDD.....**...DDDDDDDDDD.....**...BBBBBBBBBB.....**")
+    print()
+ 
 
 main()
+  
+
+
+
+
